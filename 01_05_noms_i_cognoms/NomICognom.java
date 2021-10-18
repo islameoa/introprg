443527a72473a70d8f61e17c7bad885457fa85dc
Islam <islameoa@gmail.com>
2021-10-18 12:20:33 +0200

    Del exercici 1 al 7

---
+++ 01_05_noms_i_cognoms/NomICognom.java
@@ -0,0 +1,13 @@
+public class NomICognom {
+    public static void main(String[] args) {
+        
+        System.out.println("Quin és el teu nom? ");
+        String nom = Entrada.readLine();
+        
+        System.out.println("Quin és el teu cognom?");
+        String cognom = Entrada.readLine();
+        
+        System.out.println("Ei " + nom + ", tinc una amiga que també és " + cognom);
+        
+        }
+    }
