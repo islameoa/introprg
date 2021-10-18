a7a47dba69fcba2328c32e5cfc042af141aa5450
Islam <islameoa@gmail.com>
2021-10-18 14:50:16 +0200

    Ejercicio 9 hecho

---
+++ 01_08_calculadora_basica/CalculadoraBasica.java
@@ -0,0 +1,21 @@
+public class CalculadoraBasica {
+    public static void main(String[] args){
+    // obté operands d'entrada
+        System.out.println("Primer operand:");
+        int primerOperand = Integer.parseInt(Entrada.readLine());
+        System.out.println("Segon operand:");
+        int segonOperand = Integer.parseInt(Entrada.readLine());
+
+        // operacions
+        int suma = primerOperand + segonOperand;
+        int resta = primerOperand - segonOperand;
+        int multiplicacio = primerOperand * segonOperand;
+        int divisio = primerOperand / segonOperand;
+
+        // mostra resultats
+        System.out.println(primerOperand + " + " + segonOperand + " = " + suma);
+        System.out.println(primerOperand + " - " + segonOperand + " = " + resta);
+        System.out.println(primerOperand + " x " + segonOperand + " = " + multiplicacio);
+        System.out.println(primerOperand + " / " + segonOperand + " = " + divisio);
+    }
+}
