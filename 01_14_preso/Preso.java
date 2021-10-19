ba9840d966e62fc59088ade98b30e8e5e9c1765a
Islam <islameoa@gmail.com>
2021-10-19 18:45:28 +0200

    Exercici 14 La Preso

---
+++ 01_14_preso/Preso.java
@@ -0,0 +1,14 @@
+public class Preso {
+    public static void main(String[] args){
+        System.out.println("Com us dieu?");
+        String nom = Entrada.readLine();
+        System.out.println("Quants anys teniu?");
+        int edat = Integer.parseInt(Entrada.readLine());
+        if (edat <= 17){
+            System.out.println("Aneu amb compte" + nom);
+            } else {
+                System.out.println("Vos ja podeu anar a la presó!");
+                System.out.println("Aneu amb compte " + nom);
+                }
+        }
+}
