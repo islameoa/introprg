4019d5a9ac063a0348d239fcc79aab4e373bb926
Islam <islameoa@gmail.com>
2021-11-03 18:58:54 +0100

    Exercici 5 corregit

--- 01_05_noms_i_cognoms/NomICognom.java
+++ 01_05_noms_i_cognoms/NomICognom.java
@@ -1,7 +1,11 @@
+/* Programa que ens demana el nom i el cognom i ens els imprimeix per pantalla
+amb una frase bonica*/
+
+
 public class NomICognom {
     public static void main(String[] args) {
         
-        System.out.println("Quin és el teu nom? ");
+        System.out.println("Quin és el teu nom?");
         String nom = Entrada.readLine();
         
         System.out.println("Quin és el teu cognom?");
