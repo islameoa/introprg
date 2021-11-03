3966535b3093539531928b4193ce4f0bfeb9ff47
Islam <islameoa@gmail.com>
2021-11-03 17:00:14 +0100

    Exercici 36 fet

---
+++ 01_36_endevina_cancel/Endevina.java
@@ -0,0 +1,31 @@
+/* Programa en el que l'usuari tractarà d'endevinar un número, amb intents
+il·limitats*/
+
+public class Endevina {
+    public static void main (String[] args){
+        
+        int numeroAEndevinar = 42;
+        int numeroUsuari = 0;
+        String stringUsuari;
+        
+        while (numeroUsuari != numeroAEndevinar){
+            System.out.println("Introdueix un nombre entre el 1 i el 100");
+            stringUsuari = Entrada.readLine();
+            
+            if (stringUsuari.isBlank()){
+                numeroUsuari = numeroAEndevinar;
+                System.out.println("Cancel·lat!");
+            } else {
+                numeroUsuari = Integer.parseInt(stringUsuari);
+                if (numeroUsuari < numeroAEndevinar){
+                System.out.println("És més gran que " + numeroUsuari);
+                } else if (numeroUsuari > numeroAEndevinar){
+                System.out.println("És més petit que " + numeroUsuari);
+                } else {
+                    numeroUsuari = numeroAEndevinar;
+                    System.out.println("Has encertat!");
+                }
+            } 
+        }   
+    }
+}
