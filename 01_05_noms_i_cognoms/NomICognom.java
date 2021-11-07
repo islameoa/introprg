045a4a2015f5e52b2492d51a2ff2250a06dff33e
Islam <islameoa@gmail.com>
2021-11-07 11:56:29 +0100

    Exercici 01_05 fet

--- 01_05_noms_i_cognoms/NomICognom.java
+++ 01_05_noms_i_cognoms/NomICognom.java
@@ -8,7 +8,7 @@ public class NomICognom {
         System.out.println("Com et dius?");
         String nom = Entrada.readLine();
         
-        System.out.println("I quin és el teu cognom?");
+        System.out.println("I quin és el teu primer cognom?");
         String cognom = Entrada.readLine();
         
         System.out.println("Ei " + nom + ", tinc una amiga que també és " + cognom);
