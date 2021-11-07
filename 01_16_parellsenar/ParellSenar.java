a81f6eda1d6f169ecf071e678956faeb82a38395
Islam <islameoa@gmail.com>
2021-11-07 12:08:57 +0100

    Exercici 01_16

--- 01_16_parellsenar/ParellSenar.java
+++ 01_16_parellsenar/ParellSenar.java
@@ -1,3 +1,9 @@
+/*Programa al que se li passa un nombre pel terminal i ens mostra si 
+
+és parell o senar*/
+
+
+
 public class ParellSenar{
     public static void main(String[] args){
         int numero = Integer.parseInt(args[0]);
