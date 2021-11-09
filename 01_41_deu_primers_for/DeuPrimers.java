b4f6848224e6306f4ffecf3461ff1f896baa9676
Islam <islameoa@gmail.com>
2021-11-09 18:21:40 +0100

    Exercici 1_41

--- 01_41_deu_primers_for/DeuPrimers.java
+++ 01_41_deu_primers_for/DeuPrimers.java
@@ -1,3 +1,9 @@
 /*Programa que imprimeix els deu primers nombres amb un for*/
 
-
+public class DeuPrimers {
+    public static void main (String[] args) {
+        for (int num = 1; num <= 10; num++){
+            System.out.println(num);
+        }
+    }
+}
