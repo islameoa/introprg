cb7d1254d018c38ef6c37e75d79350c32fc776d4
Islam <islameoa@gmail.com>
2021-11-09 19:57:58 +0100

    Exercici 01_47 fet

--- 01_47_historic_edats/HistoricEdats.java
+++ 01_47_historic_edats/HistoricEdats.java
@@ -10,22 +10,22 @@ public class HistoricEdats{
         int edat = Integer.parseInt(Entrada.readLine());
         System.out.println("any actual?");
         int any = Integer.parseInt(Entrada.readLine());
+        int anyNaix = any - edat;
+        int anyMes1 = anyNaix + 1;
                 
         if (nom.isEmpty() || (edat < 0) || (any < 1971) || (any > 2022)){
             System.out.println("Entrada errònia");
         } else if (edat == 0){
             System.out.println("Adéu " + nom);        
-        } else {
-            int anyNaix = any - edat;
-            int any1 = anyNaix + 1;
-            
+        }  else if (edat == 1){
+            System.out.println("El " + anyNaix + " va néixer");      
+        } else {           
             System.out.println("El " + anyNaix + " va néixer");
-            System.out.println("El " + any1  + " tenia 1 any");
+            System.out.println("El " + anyMes1  + " tenia 1 any");
             
             for (int i = 2; i < edat; i++){
-                any1++;
-                System.out.println("El " + any1 + " tenia " + i + " anys");
-                
+                anyMes1++;
+                System.out.println("El " + anyMes1 + " tenia " + i + " anys"); 
             }
             System.out.println("Adéu " + nom);
         }
