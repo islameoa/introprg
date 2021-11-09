c5c3983beca6c27b0d6d132023ab7b9710e02b82
Islam <islameoa@gmail.com>
2021-11-09 19:46:22 +0100

    Exercici 01_47 fet

---
+++ 01_47_historic_edats/HistoricEdats.java
@@ -0,0 +1,33 @@
+/* Programa que demana el nom d’una persona, la seva edat i l’any actual, 
+i escriu l’edat que tenia la persona cada any des del seu naixement.*/
+
+public class HistoricEdats{
+    public static void main(String[] args){
+        
+        System.out.println("nom?");
+        String nom = Entrada.readLine();
+        System.out.println("edat?");
+        int edat = Integer.parseInt(Entrada.readLine());
+        System.out.println("any actual?");
+        int any = Integer.parseInt(Entrada.readLine());
+                
+        if (nom.isEmpty() || (edat < 0) || (any < 1971) || (any > 2022)){
+            System.out.println("Entrada errònia");
+        } else if (edat == 0){
+            System.out.println("Adéu " + nom);        
+        } else {
+            int anyNaix = any - edat;
+            int any1 = anyNaix + 1;
+            
+            System.out.println("El " + anyNaix + " va nèixer");
+            System.out.println("El " + any1  + " tenia 1 any");
+            
+            for (int i = 2; i < edat; i++){
+                any1++;
+                System.out.println("El " + any1 + " tenia " + i + " anys");
+                
+            }
+            System.out.println("Adéu " + nom);
+        }
+    }
+}
