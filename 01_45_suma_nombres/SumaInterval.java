31bf757fe9def033bc2aac93ed784f70a5348e2d
Islam <islameoa@gmail.com>
2021-11-09 18:55:57 +0100

    Exercici 01_45

---
+++ 01_45_suma_nombres/SumaInterval.java
@@ -0,0 +1,18 @@
+/*Programa que demana un valor inicial i un de final i suma tots 
+els nombres entre ells */
+
+public class SumaInterval {
+    public static void main (String[] args) {
+        
+        System.out.println("Valor inicial?");  
+        int inici = Integer.parseInt(Entrada.readLine());
+        System.out.println("Valor final?");
+        int finale = Integer.parseInt(Entrada.readLine());
+        int suma = 0;
+        
+        for (int i = inici; i <= finale; i++){
+            suma += i;
+        }
+        System.out.println(suma);
+    }
+}
