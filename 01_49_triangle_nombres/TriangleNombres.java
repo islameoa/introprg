2ccfbbaf66a6f8d61cc13f45068982ab5aec3c27
Islam <islameoa@gmail.com>
2021-11-16 16:59:57 +0100

    Exercici 49 fet

---
+++ 01_49_triangle_nombres/TriangleNombres.java
@@ -0,0 +1,26 @@
+/*Programa al que se li introdueix un nombre i imprimeix un triangle amb un numbre de files
+igual al nombre introduit*/
+
+
+public class TriangleNombres {
+    public static void main (String[] args){
+        System.out.println("Valor?");
+        int valor = Integer.parseInt(Entrada.readLine());
+        int numImp = 1;
+        int max = 1;
+        
+        if ((valor>9) || (valor<1)){
+            System.out.println ("Valor inadequat");
+        } else {
+            for (int col=1; col<=valor; col++){
+                for (int fil=1; fil<=max; fil++){
+                    System.out.print(numImp + " ");
+                    numImp--;
+                }
+                System.out.println(" ");
+                max ++;
+                numImp = max;
+            }   
+        }
+    }
+}
