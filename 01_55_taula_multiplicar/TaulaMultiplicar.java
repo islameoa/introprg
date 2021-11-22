b5fc29d00aac8395045c1d780e93b933c2fc3f6b
Islam <islameoa@gmail.com>
2021-11-22 20:53:13 +0100

    Exercici 55 fet

---
+++ 01_55_taula_multiplicar/TaulaMultiplicar.java
@@ -0,0 +1,25 @@
+/* Programa que demana 2 nombres i mostra les taules d'aquests nombres 
+multiplicats pels dos nombres nombres seguents que demana*/
+
+public class TaulaMultiplicar {
+    public static void main(String[] args) {
+        int inici = Integer.parseInt(args[0]);
+        int finale = Integer.parseInt(args[1]);
+        int primer = Integer.parseInt(args[2]);
+        int limit = Integer.parseInt(args[3]);
+        int res = 0;
+        
+        if (inici > finale){
+            int var = finale;
+            finale = inici;
+            inici = var;
+        }
+        
+        for(int i=inici; i <= finale; i++){
+            for(int a=primer; a <= limit; a++){
+                res = i * a;
+                System.out.println(i + " X " + a + " = " + res);
+            }
+        }
+    }
+}
