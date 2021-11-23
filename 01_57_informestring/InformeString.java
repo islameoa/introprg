b736974fb1452b9adb574b4b73b0464679ad7a28
Islam <islameoa@gmail.com>
2021-11-23 15:22:35 +0100

    Exercici 57 fet

--- 01_57_informestring/InformeString.java
+++ 01_57_informestring/InformeString.java
@@ -11,14 +11,14 @@ public class InformeString {
         int num = Integer.parseInt(Entrada.readLine());
                
         System.out.println('"' + text1 + '"' + ".length(): " + text1.length());
-        System.out.println('"' + text1 + '"' + ".startsWith(" + text2 + "): " + text1.startsWith(text2));
-        System.out.println('"' + text1 + '"' + ".endsWith(" + text2 + "): " + text1.endsWith(text2));
-        System.out.println('"' + text1 + '"' + ".equals(" + text2 + "): " + text1.equals(text2));
-        System.out.println('"' + text1 + '"' + ".equalsIgnoreCase(" + text2 + "): " + text1.equalsIgnoreCase(text2));
+        System.out.println('"' + text1 + '"' + ".startsWith(" + '"' + text2 + '"' + "): " + text1.startsWith(text2));
+        System.out.println('"' + text1 + '"' + ".endsWith(" + '"' + text2 + '"' + "): " + text1.endsWith(text2));
+        System.out.println('"' + text1 + '"' + ".equals(" + '"' + text2 + '"' + "): " + text1.equals(text2));
+        System.out.println('"' + text1 + '"' + ".equalsIgnoreCase(" + '"' + text2 + '"' + "): " + text1.equalsIgnoreCase(text2));
         System.out.println('"' + text1 + '"' + ".isBlank(): " + text1.isBlank());
         System.out.println('"' + text1 + '"' + ".isEmpty(): " + text1.isEmpty());
         System.out.println('"' + text1 + '"' + ".charAt(" + num + "): " + text1.charAt(num));
-        System.out.println('"' + text1 + '"' + ".concat(" + text2 + "): " + text1.concat(text2));
+        System.out.println('"' + text1 + '"' + ".concat(" + '"' + text2 + '"' + "): " + text1.concat(text2));
         System.out.println('"' + text1 + '"' + ".repeat(" + num + "): " + text1.repeat(num));
         System.out.println('"' + text1 + '"' + ".toUpperCase(): " + text1.toUpperCase());
         System.out.println('"' + text1 + '"' + ".tolowerCase(): " + text1.toLowerCase());
