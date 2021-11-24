37ab1013293b0c1cff67baae785ca8d9ca32b42f
Islam <islameoa@gmail.com>
2021-11-24 11:09:59 +0100

    Exercici 66 fet

---
+++ 01_66_analisi_caracter/AnalitzaCaracter.java
@@ -0,0 +1,24 @@
+public class AnalitzaCaracter {
+    public static void main(String args[]){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        System.out.println("Posició?");
+        int pos = Integer.parseInt(Entrada.readLine());
+        
+        if (pos < 0){
+            pos = pos + text.length();
+        } 
+        if ((pos >= text.length()) || (pos < 0)){
+            System.out.println("Fora de rang");
+        } else {
+            if ((text.charAt(pos)>47) && (text.charAt(pos)<58)){
+                System.out.println("'" + text.charAt(pos) + "' és un nombre");
+            } else if (((text.charAt(pos)>64) && (text.charAt(pos)<91)) || (text.charAt(pos)>96)&&(text.charAt(pos)<123)){
+                System.out.println("'" + text.charAt(pos) + "' és una lletra");
+            } else {
+                System.out.println("'" + text.charAt(pos) + "' és una altra cosa");
+            }
+        }
+        
+    }
+}
