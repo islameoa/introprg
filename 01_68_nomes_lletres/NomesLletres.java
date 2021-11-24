ca3438101e5ce50d18a66c03da3823f0cc1a39b3
Islam <islameoa@gmail.com>
2021-11-24 12:25:52 +0100

    Exercici 68 fet

---
+++ 01_68_nomes_lletres/NomesLletres.java
@@ -0,0 +1,16 @@
+/*Programa que rep un string i en retorna només les lletres separades per comes*/
+
+public class NomesLletres {
+    public static void main(String args[]){
+    
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        
+        for (int x = 0; x < text.length(); x++){
+            char c = text.charAt(x);
+            if (Character.isLetter(c)){
+                System.out.print(c + ",");
+            }
+        }
+    }
+}
