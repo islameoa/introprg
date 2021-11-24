1cbd2ece69029c85f6eddd0c8dc794d3b65914e5
Islam <islameoa@gmail.com>
2021-11-24 12:39:37 +0100

    Exercici 68 fet

--- 01_68_nomes_lletres/NomesLletres.java
+++ 01_68_nomes_lletres/NomesLletres.java
@@ -8,7 +8,7 @@ public class NomesLletres {
         
         for (int x = 0; x < text.length(); x++){
             char c = text.charAt(x);
-            int ultimo = text.length();
+            int ultimo = text.length() - 2;
             
             if (Character.isLetter(c)){
                 if (x == ultimo){
