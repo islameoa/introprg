ab9a65fcb0749ae4d6b1fb61ff494eb79434f381
Islam <islameoa@gmail.com>
2021-11-24 14:48:09 +0100

    Exercici 70 fet

---
+++ 01_70_cadena_continua/CadenaContinua.java
@@ -0,0 +1,21 @@
+/*Programa que rep un string i un nombre i en repeteix els caracters que te 
+tantes vegades com el nombre indica*/
+
+public class CadenaContinua{
+    public static void main(String args[]){
+        
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        System.out.println("Nombre?");
+        int num = Integer.parseInt(Entrada.readLine());
+        
+        for (int x=0; x<num; x++){
+            if (x>=text.length()){
+                int pos = x-text.length();
+                System.out.print(text.charAt(pos));
+            } else {
+                System.out.print(text.charAt(x));
+            }
+        }
+    }
+}
