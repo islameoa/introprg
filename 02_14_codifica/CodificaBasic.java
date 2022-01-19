ca655a73a1597ec6bdea4f4b9079c099608fe9fd
islam <islameoa@gmail.com>
2022-01-19 17:17:18 +0100

    Exercici 02_14 fet

---
+++ 02_14_codifica/CodificaBasic.java
@@ -0,0 +1,38 @@
+/*Programa que rep un text i en codifica les minúscules cambiant-les 
+per la nostra elecció */
+
+public class CodificaBasic {
+    public static void main(String args[]){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        
+        System.out.println("Quants?");
+        int quants = Integer.parseInt(Entrada.readLine());
+        
+        if (quants < 0){
+        	System.out.println("No s'accepten números negatius");
+        } else {
+        	codifica(text, quants);
+        }
+    }
+    
+    public static void codifica(String text, int quants){
+    	for (int x=0; x<text.length(); x++){
+            int lletra = text.charAt(x);
+            
+            if ((lletra>=97)&&(lletra<=121)){
+                lletra = lletra+quants;
+                char lletraConvertida = (char)lletra;
+                System.out.print(lletraConvertida);
+            } else if (lletra==122){
+            	lletra = 'a'+ quants -1;
+            	char lletraConvertida = (char)lletra;
+                System.out.print(lletraConvertida);
+            } else {
+                char lletraConvertida = (char)lletra;
+                System.out.print(lletraConvertida);
+            }
+        }
+        System.out.println("");
+    }
+}
