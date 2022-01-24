7dc3d55adfb4a9a2629b303d24ff60a6dceac218
islam <islameoa@gmail.com>
2022-01-24 16:55:03 +0100

    Exercici 02_20 fet

--- 02_20_es_vocal/LloroVocalIniciFi.java
+++ 02_20_es_vocal/LloroVocalIniciFi.java
@@ -8,17 +8,18 @@ public class LloroVocalIniciFi{
 		while (confirmacio!=true){
 			System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
 			String text = Entrada.readLine();
-		
-			boolean vocal = UtilString.esVocal(text.charAt(0));
-			boolean vocalfi = UtilString.esVocal(text.charAt(text.length()-1));
-		
-			if (vocal || vocalfi == true){
-				System.out.println(text);
-			}
+			
 			if (text.isBlank()){
 				System.out.println("El lloro demana confirmació per finalitzar");
 				String resposta = Entrada.readLine();
 				confirmacio = UtilitatsConfirmacio.respostaABoolean(resposta);
+			} else {
+				boolean vocal = UtilString.esVocal(text.charAt(0));
+				boolean vocalfi = UtilString.esVocal(text.charAt(text.length()-1));
+			
+				if (vocal || vocalfi == true){
+					System.out.println(text);
+				}
 			}
 		}
 		System.out.println("Adéu");
