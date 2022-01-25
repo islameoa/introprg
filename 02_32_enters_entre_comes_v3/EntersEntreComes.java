f6a30e6bfd718494682cc6ef39613566485480fc
islam <islameoa@gmail.com>
2022-01-25 11:34:57 +0100

    Exercici 02_32 fet

--- 02_32_enters_entre_comes_v3/EntersEntreComes.java
+++ 02_32_enters_entre_comes_v3/EntersEntreComes.java
@@ -8,16 +8,18 @@ public class EntersEntreComes{
 			System.out.println("Res a fer");
 		} else {
 			int[] numeros = new int[quants];
-		
-			System.out.println("Valor 1?");
-			numeros[0]=Integer.parseInt(Entrada.readLine());
-			System.out.print(numeros[0]);
-			for (int i = 1; i < numeros.length; i++) {
+			
+			for (int i = 0; i < numeros.length; i++) {
+				System.out.println("");
 				System.out.println("Valor " + i+1 + "?");
 				numeros[i]=Integer.parseInt(Entrada.readLine());
-				System.out.print(", " + numeros[i]);
 			}
 			System.out.println();
+			
+			System.out.print(numeros[0]);
+			for (int i = 1; i < numeros.length; i++) {
+				System.out.print(", " + numeros[i]);
+			}			
 		}
 	}
 }
