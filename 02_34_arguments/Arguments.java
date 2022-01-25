a04841667e9b2181345f6aa26be9b9e07b0ad840
islam <islameoa@gmail.com>
2022-01-25 16:14:47 +0100

    Exercici 02_34 fet

--- 02_34_arguments/Arguments.java
+++ 02_34_arguments/Arguments.java
@@ -2,15 +2,19 @@
 
 public class Arguments{
 	public static void main(String[] args){
-		for (int i=0; i<args.length; i++){
-			System.out.print("["+i+"] " + '"' + args[i] + '"' + ":");
-			boolean esEnter = UtilString.esEnter(args[i]);
-			if (esEnter==true){
-				System.out.print(" és enter");
-			} else {
-				System.out.print(" no és enter");
+		if (args.length<0){
+			System.out.println("Cap argument");
+		} else {
+			for (int i=0; i<args.length; i++){
+				System.out.print("["+i+"] " + '"' + args[i] + '"' + ":");
+				boolean esEnter = UtilString.esEnter(args[i]);
+				if (esEnter==true){
+					System.out.print(" és enter");
+				} else {
+					System.out.print(" no és enter");
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
 	}
 }
