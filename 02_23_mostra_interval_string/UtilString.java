b0e57f419af3e2a2c5e45663cf04d11b1a2e7de9
islam <islameoa@gmail.com>
2022-01-25 10:35:58 +0100

    Exercici 02_23 fet

---
+++ 02_23_mostra_interval_string/UtilString.java
@@ -0,0 +1,76 @@
+/*Programa que retorna true si una lletra es vocal i false en cas contrari*/
+
+
+public class UtilString {
+    
+    public static boolean nomesLletres(char lletra) {
+    		String vocals = "aeiouàèéíïòóúüAEIOUÀÈÉÍÏÒÓÚÜ";
+    		for (int i = 0; i < vocals.length(); i++){
+    			if (vocals.charAt(i)==lletra){
+    				return true;
+    			}
+    		}
+    		return false;
+    }
+    public static String nomesLletres(String text){
+    		String nomesLletres = "";
+    		for (int i=0; i<text.length(); i++){
+    			char a = text.charAt(i);
+    			if (Character.isLetter(a)){
+    				nomesLletres += a;
+    			}
+    		}
+    		return nomesLletres;
+    }
+    public static String lletresSeparades(String text){
+    		String lletresSeparades = "";
+    		for (int i = 0; i< text.length(); i++){
+    			char a = text.charAt(i);
+    			if (i == text.length()-1){
+    				lletresSeparades += a;
+    			} else {
+    				lletresSeparades += a;
+    				lletresSeparades += ", ";
+    			}
+    		}
+    		return lletresSeparades;
+    		
+    }
+    
+    public static String intervalString(String text, int inici, int fi){
+    		String interval = "";
+    		if (inici>fi){
+    			if(inici>text.length()-1){
+    				inici = text.length()-1;
+    			}
+    			if (fi<0){
+    				fi = 0;
+    			}
+    			for(int i=inici; i>=fi; i--){
+    				char a = text.charAt(i);
+    				interval += a;
+    			}
+    			return interval;
+    		}
+    		if (inici<fi){
+    			if(fi>text.length()-1){
+    				fi = text.length()-1;
+    			}
+    			if (inici<0){
+    				inici = 0;
+    			}
+    			for(int i=inici; i<=fi; i++){
+    				char a = text.charAt(i);
+    				interval += a;
+    			}
+    			return interval;
+    		}
+    		return interval;
+    		
+    }
+    
+    
+    
+    
+    
+}
