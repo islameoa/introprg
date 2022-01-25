ee637a73e14e7e7979e6afbeae7a49ab06a374c6
islam <islameoa@gmail.com>
2022-01-25 10:02:41 +0100

    Exercici 02_21 fet

---
+++ 02_21_nomes_lletres/NomesLletres.java
@@ -0,0 +1,11 @@
+/*Programa que demana un string i només en retorna les lletres */
+
+public class NomesLletres{
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        String nomesLletres = UtilString.nomesLletres(text);
+        String separades = UtilString.lletresSeparades(nomesLletres);
+        System.out.println(separades);
+    }
+}
