a11dc43f22acc3105517260c4aca4b0539080b8b
islam <islameoa@gmail.com>
2022-01-25 12:51:31 +0100

    Exercici 02_33 fet

---
+++ 02_33_enters_Entre_comes_v4/EntersEntreComes.java
@@ -0,0 +1,46 @@
+/*Programa que demana el nombre de posicions de l'array, el separador i els valors que el contindràn. després ho imprimeix per pantalla*/
+
+public class EntersEntreComes{
+	public static void main(String args[]){
+		System.out.println("Quants?");
+		String quants = Entrada.readLine();
+		String numero = quants.replace(" ","");
+		
+		boolean esEnter = UtilString.esEnter(numero);
+		while (esEnter==false){
+			System.out.println("Per favor, un valor enter");
+			numero = Entrada.readLine();
+			esEnter = UtilString.esEnter(numero);
+		}
+		
+		System.out.println("Separador?");
+		String sep = Entrada.readLine();
+		char separador = sep.charAt(0);
+		
+		int nombre = Integer.parseInt(numero);	
+		int[] numeros = new int[nombre];
+		
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("");
+			int num = i+1;
+			
+			System.out.println("Valor " + num + "?");
+			String valor = Entrada.readLine();
+			esEnter = UtilString.esEnter(valor);
+			while (esEnter==false){
+				System.out.println("Per favor, un valor enter");
+				valor = Entrada.readLine();
+				esEnter = UtilString.esEnter(valor);
+			}
+			numeros[i]=Integer.parseInt(valor);
+		}
+		System.out.println();
+		
+		String imp = UtilString.entreComes(numeros, separador);
+		System.out.print(imp);
+		System.out.println();
+	}
+}
+
+
+
