7da466248ab4c89d54a2682ed60cd7bd30fbba75
islam <islameoa@gmail.com>
2022-02-15 17:06:32 +0100

    Exercici 03_05 fet

--- 03_05_lloro_memoria/Recordat.java
+++ 03_05_lloro_memoria/Recordat.java
@@ -28,7 +28,6 @@ public class Recordat {
         	System.out.println("El lloro pregunta paraula:");
 		    linia = Entrada.readLine();
 		    if(linia.isBlank()){
-        		System.out.println("D'acord");
         		break;
         	}
         	linia = linia.trim();
@@ -37,7 +36,6 @@ public class Recordat {
 		    sortida.newLine();
         }
         sortida.close();
-        
     }
 
     /**
@@ -68,53 +66,3 @@ public class Recordat {
         input.close();
     }
 }
-
-/*
-
-    public static void main(String[] args) throws IOException {
-    	String cami = "records.txt";
-    	String linia = "lloro";
-    	BufferedWriter sortida = new BufferedWriter(new FileWriter(cami));
-        while (linia.isBlank()==false){
-        	System.out.println("El lloro pregunta paraula:");
-		    linia = Entrada.readLine();
-		    if(linia.isBlank()){
-        		System.out.println("D'acord");
-        		break;
-        	}
-        	linia = linia.trim();
-		    System.out.println("El lloro registra: " + linia);
-		    sortida.write(linia);
-		    sortida.newLine();
-        }
-        sortida.close();
-        
-        String camiTornada = "records.txt";
-        FileReader fileReader = new FileReader(camiTornada);
-        BufferedReader input = new BufferedReader(fileReader);
-        int contador = 0;
-        
-        while(true){
-        	String linia2 = input.readLine();
-        	if(null == linia2){
-        		if (contador == 0){
-        			System.out.println("El lloro no recorda res");
-        		}
-        		System.out.println("Adéu");
-        		break;
-        	}
-        	if (linia2.isBlank()==false){
-        		System.out.print("El lloro recorda: ");
-        		System.out.println(linia2);
-        		contador ++;
-        	}
-        }
-        input.close();
-    }
-}
-*/
-
-
-
-
-
