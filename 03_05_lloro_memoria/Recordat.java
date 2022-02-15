6efd7bd5fd2814ffb077793a7957c32a082b7e00
islam <islameoa@gmail.com>
2022-02-15 17:05:25 +0100

    Exercici 03_05 fet

--- 03_05_lloro_memoria/Recordat.java
+++ 03_05_lloro_memoria/Recordat.java
@@ -7,6 +7,70 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 
 public class Recordat {
+
+	public static void main(String[] args) throws IOException {
+        String cami = "records.txt";
+        processaEntrada(cami);
+        System.out.println("D'acord");
+        mostraRecords(cami);
+    }
+
+    /**
+     * Demana frases per entrada estàndard i les guarda a un fitxer
+     * @param nomFitxer: nom del fitxer
+     */
+    public static void processaEntrada(final String nomFitxer) throws IOException {
+        // XXX: aquí la part d'escriure les memòries
+        //      No oblidaràs tancar en sortir, oi?
+        String linia = "lloro";
+    	BufferedWriter sortida = new BufferedWriter(new FileWriter(nomFitxer));
+        while (linia.isBlank()==false){
+        	System.out.println("El lloro pregunta paraula:");
+		    linia = Entrada.readLine();
+		    if(linia.isBlank()){
+        		System.out.println("D'acord");
+        		break;
+        	}
+        	linia = linia.trim();
+		    System.out.println("El lloro registra: " + linia);
+		    sortida.write(linia);
+		    sortida.newLine();
+        }
+        sortida.close();
+        
+    }
+
+    /**
+     * Mostra el contingut del fitxer amb nom nomFitxer
+     * @param nomFitxer: el nom del fitxer del que es mostrarà el contingut
+     */
+    public static void mostraRecords(final String nomFitxer) throws IOException {
+        // XXX: aquí la part de recordar les memòries escrites
+        FileReader fileReader = new FileReader(nomFitxer);
+        BufferedReader input = new BufferedReader(fileReader);
+        int contador = 0;
+        
+        while(true){
+        	String linia2 = input.readLine();
+        	if(null == linia2){
+        		if (contador == 0){
+        			System.out.println("El lloro no recorda res");
+        		}
+        		System.out.println("Adéu");
+        		break;
+        	}
+        	if (linia2.isBlank()==false){
+        		System.out.print("El lloro recorda: ");
+        		System.out.println(linia2);
+        		contador ++;
+        	}
+        }
+        input.close();
+    }
+}
+
+/*
+
     public static void main(String[] args) throws IOException {
     	String cami = "records.txt";
     	String linia = "lloro";
@@ -48,7 +112,7 @@ public class Recordat {
         input.close();
     }
 }
-
+*/
 
 
 
