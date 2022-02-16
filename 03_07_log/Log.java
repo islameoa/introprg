2c6a39a4d2bc25277e7cfd6da46f98545c0f2f5e
islam <islameoa@gmail.com>
2022-02-16 13:25:10 +0100

    Exercici 03_07

---
+++ 03_07_log/Log.java
@@ -0,0 +1,7 @@
+/*Programa que ens ofereix un registre de log, un registre dels errors del nostre programa, en aquest fitxer només tenim les 4 funcions que utilitzarà el log real*/
+
+public class Log{
+	public static printError(){
+		
+	}
+}
