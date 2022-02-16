5eaef09316b9c45ccea0e8bd1cd18f18033cde32
islam <islameoa@gmail.com>
2022-02-16 19:09:42 +0100

    Exercici 03_07 fet

--- 03_07_log/Log.java
+++ 03_07_log/Log.java
@@ -1,7 +1,48 @@
 /*Programa que ens ofereix un registre de log, un registre dels errors del nostre programa, en aquest fitxer només tenim les 4 funcions que utilitzarà el log real*/
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
 public class Log{
-	public static printError(){
-		
+	private static int contador;
+	public static void reset(int contador){
+		contador = 0;
+	}
+	public static void printError(String mssg) throws IOException{
+		String cami = "log.txt";
+		BufferedWriter sortida = new BufferedWriter(new FileWriter(cami));
+		String logError = String.format("[%d] ERROR: %s", contador, mssg);
+		sortida.write(logError);
+		sortida.newLine();
+		sortida.close();
+	}
+	public static void printWarning(String mssg) throws IOException{
+		String cami = "log.txt";
+		BufferedWriter sortida = new BufferedWriter(new FileWriter(cami));
+		String logWarning = String.format("[%d] WARNING: %s", contador, mssg);
+		sortida.write(logWarning);
+		sortida.newLine();
+		sortida.close();
+	}
+	
+	public static void printInfo(String mssg) throws IOException{
+		String cami = "log.txt";
+		BufferedWriter sortida = new BufferedWriter(new FileWriter(cami));
+		String logInfo = String.format("[%d] INFO: %s", contador, mssg);
+		sortida.write(logInfo);
+		sortida.newLine();
+		sortida.close();
+	}
+	
+	public static void printDebug(String mssg) throws IOException{
+		String cami = "log.txt";
+		BufferedWriter sortida = new BufferedWriter(new FileWriter(cami));
+		String logDebug = String.format("[%d] DEBUG: %s", contador, mssg);
+		sortida.write(logDebug);
+		sortida.newLine();
+		sortida.close();
 	}
 }
