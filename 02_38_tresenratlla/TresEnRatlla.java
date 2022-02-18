f2cf70197f9d6dbdac2c89b38592ca23ae045408
islam <islameoa@gmail.com>
2022-02-18 19:41:37 +0100

    Islikks

---
+++ 02_38_tresenratlla/TresEnRatlla.java
@@ -0,0 +1,159 @@
+/* XXX comentari adequat */
+public class TresEnRatlla {
+
+    // mòduls de suport
+    
+    public static boolean casellaOcupada(char[][] taulell, int fila, int columna){
+    	if (taulell[fila][columna] == '·'){
+    		return false;	
+		} else {
+			return true;
+		}
+    }
+    public static void mostraTaulell(char[][] taulell){
+    		for(int fila=0; fila<taulell.length; fila++){
+    			for(int col=0; col<taulell[0].length; col++){
+    				System.out.print(taulell[fila][col]);
+    			}
+    			System.out.println();
+    		}
+    }
+        
+    public static boolean jugadorGuanya(char[][] taulell, char jugador){
+    		int contador = 0;
+    		for(int fila=0; fila<taulell.length; fila++){
+    			for(int col=0; col<taulell[0].length; col++){
+    				if (taulell[fila][col]==jugador){
+    					contador++;
+    				} else {
+    					contador = 0;
+    				}
+    			}
+    			if (contador == 3){
+    				return true;
+    			}                                                                               
+    		}
+    		for(int col=0; col<3; col++){
+    			for(int fila=0; fila<taulell.length; fila++){
+				if (taulell[fila][col]==jugador){
+					contador++;
+				} else {
+					contador = 0;
+				}
+				if (contador == 3){
+					return true;
+				}
+			}
+    		}
+    		for(int fila=0; fila<taulell.length; fila++){
+    			for(int col=0; col<taulell[0].length; col++){
+    				if (taulell[fila][col]==jugador && fila==col){
+    					contador++;
+    				}
+    			}
+    			if (contador == 3){
+    				return true;
+    			}                                                                
+    		}
+    		for(int fila=0; fila<taulell.length; fila++){
+    			for(int col=0; col<taulell[0].length; col++){
+    				if ((taulell[fila][col]==jugador) && ((fila==0 && col==2) || (fila==1 && col==1) || (fila==2 && col==0))){
+    					contador++;
+    				}
+    			}
+    			if (contador == 3){
+    				return true;
+    			}                                                                
+    		}
+    		return false;
+    }
+    public static boolean hiHaEmpat(char[][] taulell){
+    	for(int fila=0; fila<taulell.length; fila++){
+    			for(int col=0; col<taulell[0].length; col++){
+    				if (taulell[fila][col]=='·'){
+    					return false;
+    				}
+    			}
+    		}
+    	return false;
+    }
+
+    public static void main(String[] args) {
+        // declara i inicialitza el taulell
+        char[][] taulell = new char[3][3];
+        for(int fila=0; fila<taulell.length; fila++){
+			for(int col=0; col<taulell[0].length; col++){
+				taulell[fila][col] = '·';
+			}
+		}
+
+        System.out.println("Comença el joc");
+
+        // indica quin és el jugador que té el torn
+        char jugador = 'X';
+        int valor1 = 3;
+        int valor2 = 3;
+        boolean guanyador = false;
+        boolean empat = false;
+
+        while (!guanyador && !empat) {
+        
+            mostraTaulell(taulell);
+
+            // obté el moviment del jugador actual
+            String entrada = Entrada.readLine();
+            boolean entradaCorrecta = false;
+            boolean ocupada = true;
+            
+            
+            while(!entradaCorrecta && ocupada){
+            	if (entrada == "a"){
+            		System.out.println(jugador + " abandona");
+            		return;
+            	}
+            	if (entrada.length()!=2){
+					entrada = Entrada.readLine();
+				} else {
+					String uno = "" + entrada.charAt(0);
+					String dos = "" + entrada.charAt(1);
+					valor1 = Integer.parseInt(uno);
+					valor2 = Integer.parseInt(dos);
+					
+					if (valor1!=0 && valor1!=1 && valor1!=2 && valor2!=0 && valor2!=1 && valor2!=2){
+						System.out.println("Error");
+						entrada = Entrada.readLine();
+					} else {
+						entradaCorrecta = true;
+					}
+					if (entradaCorrecta){
+						ocupada = casellaOcupada(taulell, valor1, valor2);
+						
+						if (ocupada){
+							System.out.println("ocupada");
+							entrada = Entrada.readLine();
+						}
+					}
+				}
+            }           
+
+            // realitza el moviment
+            taulell[valor1][valor2] = jugador;
+
+            // comprova jugador guanya
+            guanyador = jugadorGuanya(taulell, jugador);
+            if (guanyador){
+            	System.out.println("Ha guanyat " + jugador);
+            }
+
+            // comprova empat
+            empat = hiHaEmpat(taulell);
+
+            // passa torn a l'altre jugador
+            if (jugador =='X'){
+            	jugador = 'O';
+            } else {
+            	jugador = 'X';
+            }
+        }
+    }
+}
