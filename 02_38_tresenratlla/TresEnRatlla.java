16e6d0b2df6d686cff9481838f446584fe9a862b
islam <islameoa@gmail.com>
2022-02-20 17:43:14 +0100

    EXERCICI DEL 3 EN RATLLA

--- 02_38_tresenratlla/TresEnRatlla.java
+++ 02_38_tresenratlla/TresEnRatlla.java
@@ -1,14 +1,15 @@
-/* XXX comentari adequat */
+/* En aquest exercici implementarem el joc del tres en ratlla de manera que es pugui jugar. 
+El joc començarà amb el taulell en blanc i anirà demanant moviments alternativament a cada jugador. 
+Finalitza quan un dels dos guanya, hi ha empat o un dels dos escriu a*/
 public class TresEnRatlla {
 
     // mòduls de suport
     
     public static boolean casellaOcupada(char[][] taulell, int fila, int columna){
-    	if (taulell[fila][columna] == '·'){
+    	if (taulell[fila][columna] == '.'){
     		return false;	
-		} else {
-			return true;
 		}
+		return true;
     }
     public static void mostraTaulell(char[][] taulell){
     		for(int fila=0; fila<taulell.length; fila++){
@@ -33,18 +34,22 @@ public class TresEnRatlla {
     				return true;
     			}                                                                               
     		}
-    		for(int col=0; col<3; col++){
-    			for(int fila=0; fila<taulell.length; fila++){
-				if (taulell[fila][col]==jugador){
-					contador++;
-				} else {
-					contador = 0;
+    		contador = 0;
+    		for(int col=0; col<taulell[0].length; col++){
+				contador=0;
+				for(int fila=0; fila<taulell.length; fila++){
+					if (taulell[fila][col]==jugador){
+						contador++;
+					} else {
+    					contador = 0;
+    				}
+
+					if (contador == 3){
+						return true;
+					}
 				}
-				if (contador == 3){
-					return true;
-				}
-			}
     		}
+    		contador = 0;
     		for(int fila=0; fila<taulell.length; fila++){
     			for(int col=0; col<taulell[0].length; col++){
     				if (taulell[fila][col]==jugador && fila==col){
@@ -55,6 +60,7 @@ public class TresEnRatlla {
     				return true;
     			}                                                                
     		}
+    		contador = 0;
     		for(int fila=0; fila<taulell.length; fila++){
     			for(int col=0; col<taulell[0].length; col++){
     				if ((taulell[fila][col]==jugador) && ((fila==0 && col==2) || (fila==1 && col==1) || (fila==2 && col==0))){
@@ -63,19 +69,19 @@ public class TresEnRatlla {
     			}
     			if (contador == 3){
     				return true;
-    			}                                                                
+    			}                                                           
     		}
     		return false;
     }
     public static boolean hiHaEmpat(char[][] taulell){
-    	for(int fila=0; fila<taulell.length; fila++){
-    			for(int col=0; col<taulell[0].length; col++){
-    				if (taulell[fila][col]=='·'){
-    					return false;
-    				}
-    			}
-    		}
-    	return false;
+    	for(int fila=0; fila<3; fila++){
+			for(int col=0; col<3; col++){
+				if (taulell[col][fila]=='.'){
+					return false;
+				}
+			}
+		}
+    	return true;
     }
 
     public static void main(String[] args) {
@@ -83,7 +89,7 @@ public class TresEnRatlla {
         char[][] taulell = new char[3][3];
         for(int fila=0; fila<taulell.length; fila++){
 			for(int col=0; col<taulell[0].length; col++){
-				taulell[fila][col] = '·';
+				taulell[fila][col] ='.';
 			}
 		}
 
@@ -99,40 +105,47 @@ public class TresEnRatlla {
         while (!guanyador && !empat) {
         
             mostraTaulell(taulell);
-
+			System.out.println(jugador + "?");
             // obté el moviment del jugador actual
-            String entrada = Entrada.readLine();
+            
             boolean entradaCorrecta = false;
             boolean ocupada = true;
             
             
-            while(!entradaCorrecta && ocupada){
-            	if (entrada == "a"){
+            while(!entradaCorrecta || ocupada){
+            	String entrada = Entrada.readLine();
+            	if (entrada.equals("a")){
             		System.out.println(jugador + " abandona");
             		return;
             	}
             	if (entrada.length()!=2){
-					entrada = Entrada.readLine();
+            		System.out.println("Error");
+					//entrada = Entrada.readLine();
 				} else {
 					String uno = "" + entrada.charAt(0);
 					String dos = "" + entrada.charAt(1);
-					valor1 = Integer.parseInt(uno);
-					valor2 = Integer.parseInt(dos);
-					
-					if (valor1!=0 && valor1!=1 && valor1!=2 && valor2!=0 && valor2!=1 && valor2!=2){
+					if (((uno.charAt(0) < 48) || (uno.charAt(0)>57)) || ((dos.charAt(0) < 48) || (dos.charAt(0)>57))){
 						System.out.println("Error");
-						entrada = Entrada.readLine();
 					} else {
-						entradaCorrecta = true;
+						valor1 = Integer.parseInt(uno);
+						valor2 = Integer.parseInt(dos);
+						
+						if ((valor1!=0 && valor1!=1 && valor1!=2) || (valor2!=0 && valor2!=1 && valor2!=2)){
+							System.out.println("Error");
+							//entrada = Entrada.readLine();
+						} else {
+							entradaCorrecta = true;
+						}
 					}
-					if (entradaCorrecta){
+				}
+				if (entradaCorrecta){
 						ocupada = casellaOcupada(taulell, valor1, valor2);
 						
 						if (ocupada){
-							System.out.println("ocupada");
-							entrada = Entrada.readLine();
+							ocupada = true;
+							System.out.println("Ocupada");
+							//entrada = Entrada.readLine();
 						}
-					}
 				}
             }           
 
@@ -142,11 +155,15 @@ public class TresEnRatlla {
             // comprova jugador guanya
             guanyador = jugadorGuanya(taulell, jugador);
             if (guanyador){
+            	mostraTaulell(taulell);
             	System.out.println("Ha guanyat " + jugador);
             }
 
             // comprova empat
             empat = hiHaEmpat(taulell);
+            if (empat){
+            	System.out.println("Empat");
+            }
 
             // passa torn a l'altre jugador
             if (jugador =='X'){
@@ -155,5 +172,6 @@ public class TresEnRatlla {
             	jugador = 'X';
             }
         }
+
     }
 }
