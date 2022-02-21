55181841cf83b0e31bcc02769406e49bd17efb6e
islam <islameoa@gmail.com>
2022-02-21 17:31:12 +0100

    Exercici del penjat

--- 03_04_elpenjat/Penjat.java
+++ 03_04_elpenjat/Penjat.java
@@ -3,6 +3,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class Penjat{
 	public static void main(String[] args) throws IOException{
@@ -27,13 +29,17 @@ public class Penjat{
         		break;
         	}
         	boolean noEncertada = true;
+        	boolean lletraRepetida = true;
         	char [] paraulaU = new char[paraulaSys.length()];
+        	int intentsDisponibles = 10;
+        	boolean lletraFallada;
+        	
         	for (int x =0; x<paraulaSys.length(); x++){
         		paraulaU[x]='*';
         	}
         	
 			String lletresUsades = "";
-        	while(noEncertada || confirmacio){
+        	while(noEncertada || confirmacio || intentsDisponibles>0){
         		
 		    	System.out.println("Introdueix una lletra");
 		    	String lletraActual = Entrada.readLine();
@@ -43,14 +49,17 @@ public class Penjat{
 		    		confirmacio = UtilitatsConfirmacio.respostaABoolean(resposta);
 		    	} else if (lletraActual.equals("glups")){
 		    		noEncertada = false;
-		    	} else if (lletraActual.charAt(0)<65 || lletraActual.charAt(0)>123){
+		    	} else if (lletraActual.charAt(0)<65 || lletraActual.charAt(0)>123 || lletraActual.length()>1){
 		    		System.out.println("Error: cal una lletra entre 'a' i 'z', 'prou' o 'glups'");
 		    	} else {
+		    	lletraRepetida = lletraRepetida(lletraActual.charAt(0), lletresUsades);
+		    	if (!lletraRepetida){
 		    		if (contador==0){
 						lletresUsades = lletraActual;
 					} else {
 						lletresUsades += lletraActual;
 					}
+					
 					lletresUsades = lletresUsades.toUpperCase();
 					contador++;
 					char lletraActualC = lletraActual.charAt(0);
@@ -66,31 +75,47 @@ public class Penjat{
 							}
 						}
 					}
-					System.out.print("Paraula: ");
-					mostraParaula(paraulaU);
-					System.out.print("Utilitzades: ");
 					
-					if (lletresUsades.length()==0){
-						System.out.print("cap");
+					noEncertada = paraulaEncertada(paraulaU, paraulaSys);
+					if(!noEncertada){
+						System.out.println("Has encertat! La paraula era " + paraulaSys);
 					} else {
-						for (int u=0; u<lletresUsades.length(); u++){
-							
-							System.out.print(lletresUsades.charAt(u));
-							if (u==lletresUsades.length()-2){
-								System.out.print(" i ");
-							} else if (u<lletresUsades.length()-2){
-								System.out.print(", ");
+						System.out.print("Paraula: ");
+						mostraParaula(paraulaU);
+						System.out.print("Utilitzades: ");
+						
+						if (lletresUsades.length()==0){
+							System.out.print("cap");
+						} else {
+							for (int u=0; u<lletresUsades.length(); u++){
+								
+								System.out.print(lletresUsades.charAt(u));
+								if (u==lletresUsades.length()-2){
+									System.out.print(" i ");
+								} else if (u<lletresUsades.length()-2){
+									System.out.print(", ");
+								}
 							}
 						}
+						System.out.println("");
+						System.out.print("Intents disponibles: ");
+						String paraulaUString = "";
+						for (int i=0; i<paraulaU.length; i++){
+							paraulaUString += paraulaU[i];
+						}
+						lletraFallada = paraulaUString.contains(lletraActual);
+						if (intentsDisponibles<10 && !lletraFallada){
+							mostraFigura(intentsDisponibles);
+						}
+						intentsDisponibles--;
 					}
-					System.out.println("");
-					System.out.print("Intents disponibles: ");
-					noEncertada = paraulaEncertada(paraulaU, paraulaSys);
+					//lletraRepetida = true;
 		    	}
         	}
         }
 	}
-	
+	input.close();
+	}
 	public static void mostraParaula(char[] paraula){
 		for (int i=0; i<paraula.length; i++){
 			System.out.print(paraula[i]);
@@ -108,8 +133,54 @@ public class Penjat{
 		}
 		return true;
 	}
-	public static void mostraFigura(int intentsDisponibles){
-		
+	public static void mostraFigura(int intentsDisponibles) throws IOException{
+		int intentos=0;
+		if (intentsDisponibles == 8){
+			intentos = 1;
+		}
+		if (intentsDisponibles == 7){
+			intentos = 2;
+		}
+		if (intentsDisponibles == 6){
+			intentos = 3;
+		}
+		if (intentsDisponibles == 5){
+			intentos = 4;
+		}
+		if (intentsDisponibles == 4){
+			intentos = 5;
+		}
+		if (intentsDisponibles == 3){
+			intentos = 6;
+		}
+		if (intentsDisponibles == 2){
+			intentos = 7;
+		}
+		if (intentsDisponibles == 1){
+			intentos = 8;
+		}
+		if (intentsDisponibles == 0){
+			intentos = 9;
+		}
+		FileReader fileReader = new FileReader("./recursos/figura"+intentos+".txt");
+        BufferedReader input = new BufferedReader(fileReader);
+                
+        while(true){
+        	String linia = input.readLine();
+        	if(null == linia){
+        		break;
+        	}
+        	System.out.println(linia);
+        }
+        input.close();
+	}
+	public static boolean lletraRepetida(char lletraActual, String lletresUsades){
+		for(int j=0; j<lletresUsades.length(); j++){
+			if (lletresUsades.charAt(j) == lletraActual){
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
