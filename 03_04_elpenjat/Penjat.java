7ad2e9a5b5ccebdee7eb631fe888b33512cb1aaa
islam <islameoa@gmail.com>
2022-02-21 11:44:53 +0100

    Exercici del penjat

--- 03_04_elpenjat/Penjat.java
+++ 03_04_elpenjat/Penjat.java
@@ -13,6 +13,8 @@ public class Penjat{
 		FileReader fileReader = new FileReader(fitxer);
 		BufferedReader input = new BufferedReader(fileReader);
 		
+		System.out.println("Comencem a jugar");
+		
 		while(hiHaParaules){
 			String paraulaSys = input.readLine();
         	if (null == paraulaSys){
@@ -25,28 +27,55 @@ public class Penjat{
         	}
         	boolean noEncertada = true;
         	char [] paraulaU = new char[paraulaSys.length()];
+        	for (int x =0; x<paraulaSys.length(); x++){
+        		paraulaU[x]='*';
+        	}
+        	
+			String lletresUsades = "";
         	while(noEncertada){
-        		String lletresUsades = "";
+        		
 		    	System.out.println("Introdueix una lletra");
 		    	String lletraActual = Entrada.readLine();
-		    	lletresUsades += lletraActual;
-		    	lletresUsades = lletresUsades.toUpperCase();
+		    	if (contador==0){
+		    		lletresUsades = lletraActual;
+		    		lletresUsades = lletresUsades.toUpperCase();
+		    	} else {
+		    		lletresUsades += lletraActual;
+		    		lletresUsades = lletresUsades.toUpperCase();
+		    	}
+		    	contador++;
 		    	char lletraActualC = lletraActual.charAt(0);
 		    	
 		    	for (int i=0; i<paraulaSys.length(); i++){
 					if (lletraActualC == paraulaSys.charAt(i)){
 						paraulaU[i] = Character.toLowerCase(lletraActualC);
-					} else if (noEncertada){
+					} else {
 						for(int j=0; j<lletresUsades.length(); j++){
 							if (lletresUsades.charAt(j) == paraulaSys.charAt(i)){
 								paraulaU[i] = lletresUsades.charAt(j);
 							}
 						}
-					} else {
-						paraulaU[i] = '*';
 					}
 		    	}
+		    	System.out.print("Paraula: ");
 		    	mostraParaula(paraulaU);
+		    	System.out.print("Utilitzades: ");
+		    	
+		    	if (lletresUsades.length()==0){
+		    		System.out.print("cap");
+		    	} else {
+		    		for (int u=0; u<lletresUsades.length(); u++){
+		    			
+		    			System.out.print(lletresUsades.charAt(u));
+		    			if (u==lletresUsades.length()-2){
+		    				System.out.print(" i ");
+		    			} else if (u<lletresUsades.length()-2){
+		    				System.out.print(", ");
+		    			}
+		    		}
+		    	}
+		    	System.out.println("");
+		    	noEncertada = paraulaEncertada(paraulaU, paraulaSys);
         	}
         }
 	}
@@ -56,6 +85,17 @@ public class Penjat{
 			System.out.print(paraula[i]);
 		}
 		System.out.println("");
+	}
+	
+	public static boolean paraulaEncertada(char[] paraula, String paraulaSys){
+		String compare = "";
+		for (int i=0; i<paraula.length; i++){
+			compare += paraula[i];
+		}
+		if (compare.equals(paraulaSys)){
+			return false;
+		}
+		return true;
 	}
 }
 
