8d7c657d01af937109c829d66f70953874cc0c71
islam <islameoa@gmail.com>
2022-03-01 16:31:15 +0100

    Exercici 04_04 fet

--- 04_04_pujapis/Ascensor.java
+++ 04_04_pujapis/Ascensor.java
@@ -1,18 +1,18 @@
 /*Creació de la classe ascensor amb el mòdul pujaPis per tal de que a la seva variable pis se li sumi 1*/
 
-public class pujaPis {
+public class Ascensor {
     public int pis = -1;
     
-    public static void pujaPis(pujaPis ascensor) {
+    public static void Ascensor(Ascensor ascensor) {
         ascensor.pis++;
     }
     public static void main(String[] args){
-        pujaPis ascensor;           // declarem la referència a l'ascensor
-        ascensor = new pujaPis();   // creem la instància de l'ascensor
+        Ascensor ascensor;           // declarem la referència a l'ascensor
+        ascensor = new Ascensor();   // creem la instància de l'ascensor
         
         System.out.println("L'ascensor inicialment està a la planta " + ascensor.pis);
         
-        pujaPis(ascensor);
+        Ascensor(ascensor);
         
         System.out.println("L'ascensor finalment està a la planta " + ascensor.pis);
     }
