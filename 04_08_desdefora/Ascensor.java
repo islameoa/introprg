0d67f79ba3140eda2728fbb643f8817921810f1d
islam <islameoa@gmail.com>
2022-03-06 13:35:34 +0100

    Exercici 04_07 fet

---
+++ 04_08_desdefora/Ascensor.java
@@ -0,0 +1,5 @@
+/*Creació de la classe ascensor que inicialitza el pis a -1*/
+
+public class Ascensor {
+    public int pis = -1;         // pis inicial de l'ascnesor
+}
--- 04_08_desdefora/UsaAscensor.java
+++ 04_08_desdefora/UsaAscensor.java
@@ -1,1 +1,8 @@
-/*Programa ue */
+/*Programa que utilitzarà la classe ascensor per tal de comprovar que es pot executar una classe des de fora.*/
+
+public class UsaAscensor{
+	public static void main(String args[]){
+		Ascensor ascensor = new Ascensor();
+		System.out.println("L'ascensor està a la planta " + ascensor.pis);
+	}
+}
