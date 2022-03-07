c9b2cd3324b6fc638d3c39d73600ec08ee82537d
islam <islameoa@gmail.com>
2022-03-07 18:35:38 +0100

    Exercici 04_12 fet

---
+++ 04_12_posicio_correcta/GatRenat.java
@@ -0,0 +1,26 @@
+/*Classe GatRenat que inicialitza les seves instàncies amb 7 vides i assegudes*/
+
+public class GatRenat {
+    private int vides = 7;         // vides disponibles del gat Renat
+    private String posicio = "estirat";
+
+	public int GetVides(){
+		return vides;
+	}
+
+	public void setVides(int novesVides) {   // modifica el nombre de vides si ens donen un de vàlid
+		       if (novesVides >= 0) {
+		           vides = novesVides;
+		       }
+		   }
+		   
+	public void setPosicio(String posicio2){
+		if(posicio2.equals("assegut") || posicio2.equals("estirat") || posicio2.equals("dret")){
+			posicio = posicio2;
+		}
+	}
+	
+	public String GetPosicio(){
+		return posicio;
+	}	
+}
