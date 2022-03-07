781e8ff935e418fb6fbdf4a7e5e8bc4af261c6de
islam <islameoa@gmail.com>
2022-03-07 18:37:28 +0100

    Exercici 04_12 fet

--- 04_12_posicio_correcta/UsaGatRenat.java
+++ 04_12_posicio_correcta/UsaGatRenat.java
@@ -3,10 +3,10 @@
 public class UsaGatRenat {
 	public static void main(String[] args) {
 		GatRenat renat = new GatRenat();
-		System.out.println("Vides inicials: " + renat.GetVides());
-		System.out.println("Posició inicial: " + renat.GetPosicio());
+		System.out.println("Vides inicials: " + renat.getVides());
+		System.out.println("Posició inicial: " + renat.getPosicio());
 		System.out.println("Introdueix nova posició:");
 		renat.setPosicio(Entrada.readLine());
-		System.out.println("Posició final: " + renat.GetPosicio());
+		System.out.println("Posició final: " + renat.getPosicio());
 	}
 }
