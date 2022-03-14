2b8aea7326cc8de241d6016411d1c02bdcf593db
islam <islameoa@gmail.com>
2022-03-14 18:36:13 +0100

    Exercici 04_23 fet

---
+++ 04_23_aquesta_posicio/GatRenat.java
@@ -0,0 +1,25 @@
+/*Exercici en el que passarem les propietats de la classe com a paramàtres del constructor*/
+
+public class GatRenat {
+    private int vides;
+    private String posicio;
+    
+    public GatRenat(int novesVides, String posicio) {
+        vides = novesVides;
+    }
+    @Override
+    public String toString() {
+        return String.format("Vides: %d. Posició: %s", vides, posicio);
+    }
+    public int getVides() { return vides; }
+    public String getPosicio() { return posicio; }
+    
+    public void setPosicio(String posicio){
+    	posicio = posicio;
+    }
+    
+    public static void main(String[] args) {
+        GatRenat renat = new GatRenat(7, "estirat");
+        System.out.println(renat);
+    }
+}
