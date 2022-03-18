1920cf928970ef7230ee92212893e8221f87a5a8
islam <islameoa@gmail.com>
2022-03-18 15:58:43 +0100

    Exercici 04_25 fet

--- 04_25_hora/Hora.java
+++ 04_25_hora/Hora.java
@@ -91,24 +91,24 @@ public class Hora {
         // }
         // }
         // }
-        if (segons < 0) {
-            segons = abs(segons);
-            while (segons > 0) {
-                this.segons--;
-                if (this.segons < 0) {
-                    this.segons = 59;
-                    minuts--;
-                    if (minuts < 0) {
-                        minuts = 59;
-                        hores--;
-                        if (hores < 0) {
-                            hores = 23;
-                        }
-                    }
-                }
-                segons--;
-            }
-        } else {
+        // if (segons < 0) {
+        //     segons = abs(segons);
+        //     while (segons > 0) {
+        //         this.segons--;
+        //         if (this.segons < 0) {
+        //             this.segons = 59;
+        //             minuts--;
+        //             if (minuts < 0) {
+        //                 minuts = 59;
+        //                 hores--;
+        //                 if (hores < 0) {
+        //                     hores = 23;
+        //                 }
+        //             }
+        //         }
+        //         segons--;
+        //     }
+        // } else {
             this.segons += hores * 3600 + minuts * 60 + segons;
             hores = this.segons / 3600;
             while (hores > 23) {
@@ -141,7 +141,7 @@ public class Hora {
                 this.segons += res;
             }
         }
-    }
+    //}
 
     // Fa que l’hora tingui un segon menys
     void decrementa() {
@@ -162,8 +162,8 @@ public class Hora {
     // Fa que l’hora es decrementi en el nombre de segons indicats
     void decrementa(int segons) {
         if (segons < 0) {
-            int contador = abs(segons);
-            incrementa(contador);
+            segons = abs(segons);
+            incrementa(segons);
         } else {
             while (segons > 0) {
                 this.segons--;
