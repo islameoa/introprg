81a15e3101d39fde1648186af168cf43c56ff4fb
islam <islameoa@gmail.com>
2022-03-22 10:25:04 +0100

    Exercici 04_25 fet

--- 04_25_hora/Hora.java
+++ 04_25_hora/Hora.java
@@ -202,17 +202,16 @@ public class Hora {
         Hora hora1 = new Hora();
         Hora hora2 = new Hora(0, 0, 2);
         System.out.printf("Inicialment hora1: %s %s hora2: %s%n",
-        hora1,
-        composaOperadorComparacio(hora1, hora2),
-        hora2);
-        System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a
-        la segona");
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
+        System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a la segona");
         hora1.incrementa();
         hora2.decrementa();
         System.out.printf("Finalment hora1: %s %s hora2: %s%n",
-        hora1,
-        composaOperadorComparacio(hora1, hora2),
-        hora2);
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
         // Hora hora1 = new Hora(0, 0, 1);
         // hora1.decrementa(1);
         // System.out.printf("Finalment hora1: %s ", hora1);
