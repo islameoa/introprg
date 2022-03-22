bf16d8304d8e2a95f6c3721db54f16ac6fa4f00e
islam <islameoa@gmail.com>
2022-03-22 11:11:04 +0100

    Exercici 04_25 fet

--- 04_25_hora/Hora.java
+++ 04_25_hora/Hora.java
@@ -126,83 +126,86 @@ public class Hora {
             segons = abs(segons);
             incrementa(segons);
         } else {
-            int segInicials = hores * 3600 + minuts * 60 + this.segons;
-            segons = segInicials - segons;
-            if (segons >= 0) {
-                hores = segons / 3600;
-                while (hores > 24) {
-                    hores -= 24;
-                }
-                segons %= 3600;
-                minuts = segons / 60;
-                while (minuts > 60) {
-                    minuts -= 60;
-                }
-                this.segons = segons % 60;
-            } else {
-                if (segons > -1000) {
-                    while (segons < 0) {
-                        this.segons--;
-                        if (this.segons < 0) {
-                            this.segons = 59;
-                            minuts--;
-                            if (minuts < 0) {
-                                minuts = 59;
-                                hores--;
-                                if (hores < 0) {
-                                    hores = 23;
-                                }
-                            }
-                        }
-                        segons++;
-                    }
-                } else {
-                    hores = segons / 3600;
-                    float resto = segons % 3600;
-                    while (hores <= -24) {
-                        hores += 24;
-                    }
-                    if (hores < 0 && resto == 0) {
-                        hores = 24 + hores;
-                    }
-                    if (hores < 0 && resto != 0) {
-                        hores = 23 + hores;
-                    }
-                    segons %= 3600;
-                    minuts = segons / 60;
-                    while (minuts < 0) {
-                        minuts += 60;
-                    }
-                    segons %= 60;
-                    while (segons < 0) {
-                        this.segons--;
-                        if (this.segons < 0) {
-                            this.segons = 59;
-                            minuts--;
-                            if (minuts < 0) {
-                                minuts = 59;
-                                hores--;
-                                if (hores < 0) {
-                                    hores = 23;
-                                }
-                            }
-                        }
-                        segons++;
-                    }
-                    // while (segons > 60) {
-                    //     this.segons -= 60;
-                    //     minuts--;
-                    //     if (this.segons < 0) {
-                    //         this.segons = 0;
-                    //     }
-                    //     segons--;
-                    // }
-                    // while (segons > 0) {
-                    //     this.segons--;
-                    //     segons--;
-                    // }
-                }
+            int hora2 = segons / 3600;
+            while (hora2 >= 24) {
+                hora2 -= 24;
             }
+            segons %= 3600;
+            int minuts2 = segons / 60;
+            while (minuts2 > 60) {
+                minuts2 -= 60;
+            }
+            segons = segons % 60;
+            hores -= hora2;
+            minuts -= minuts2;
+            this.segons -= segons;
+
+            //System.out.println("hora a restar: " + hora2 + ":" + minuts2 + ":" + segons);
+            // int segInicials = hores * 3600 + minuts * 60 + this.segons;
+            // segons = segInicials - segons;
+            // if (segons >= 0) {
+            //     hores = segons / 3600;
+            //     while (hores > 24) {
+            //         hores -= 24;
+            //     }
+            //     segons %= 3600;
+            //     minuts = segons / 60;
+            //     while (minuts > 60) {
+            //         minuts -= 60;
+            //     }
+            //     this.segons = segons % 60;
+            // } else {
+            //     if (segons > -1000) {
+            //         while (segons < 0) {
+            //             this.segons--;
+            //             if (this.segons < 0) {
+            //                 this.segons = 59;
+            //                 minuts--;
+            //                 if (minuts < 0) {
+            //                     minuts = 59;
+            //                     hores--;
+            //                     if (hores < 0) {
+            //                         hores = 23;
+            //                     }
+            //                 }
+            //             }
+            //             segons++;
+            //         }
+            //     } else {
+            //         hores = segons / 3600;
+            //         float resto = segons % 3600;
+            //         while (hores <= -24) {
+            //             hores += 24;
+            //         }
+            //         if (hores < 0 && resto == 0) {
+            //             hores = 24 + hores;
+            //         }
+            //         if (hores < 0 && resto != 0) {
+            //             hores = 23 + hores;
+            //         }
+            //         segons %= 3600;
+            //         minuts = segons / 60;
+            //         while (minuts < 0) {
+            //             minuts += 60;
+            //         }
+            //         segons %= 60;
+            //         while (segons < 0) {
+            //             this.segons--;
+            //             if (this.segons < 0) {
+            //                 this.segons = 59;
+            //                 minuts--;
+            //                 if (minuts < 0) {
+            //                     minuts = 59;
+            //                     hores--;
+            //                     if (hores < 0) {
+            //                         hores = 23;
+            //                     }
+            //                 }
+            //             }
+            //             segons++;
+            //         }
+            //     }
+            // }
         }
     }
 
@@ -250,8 +253,8 @@ public class Hora {
                 hora1,
                 composaOperadorComparacio(hora1, hora2),
                 hora2);
-        // Hora hora1 = new Hora(0, 0, 1);
-        // hora1.decrementa(1);
+        // Hora hora1 = new Hora(12, 21, 52);
+        // hora1.decrementa(432001);
         // System.out.printf("Finalment hora1: %s ", hora1);
         // System.out.println();
     }
