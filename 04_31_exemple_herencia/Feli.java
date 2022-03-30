ed602464c786db88f7be5fd52416301e5382d015
islam <islameoa@gmail.com>
2022-03-30 13:20:31 +0200

    Exercici 04_29 fet

---
+++ 04_31_exemple_herencia/Feli.java
@@ -0,0 +1,17 @@
+public class Feli {
+    private UrpaRetractil[] urpes;
+    public void caça(Animal presa) { /* … */ }
+}
+
+class Animal {
+    public String nom;
+}
+
+class Gat extends Feli {
+    
+}
+
+class Pantera extends Feli {
+    
+}
+
