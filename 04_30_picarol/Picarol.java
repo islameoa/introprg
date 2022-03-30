123d72ff63827739ca7c64587f2e10e55796f0e1
islam <islameoa@gmail.com>
2022-03-30 14:07:39 +0200

    Exercici 04_30 fet

--- 04_30_picarol/Picarol.java
+++ 04_30_picarol/Picarol.java
@@ -1,6 +1,6 @@
 /*Classe picarol que conté la propietat cops, cops que sona el picarol, metode sona per fer-lo sonar i mètode quants cops per saber el nombre de cops que ha sonat */
 public class Picarol {
-    private static int cops = 0;
+    private int cops = 0;
 
     // metodo suena que muestra el mensaje "clink-clink" en la consola
     public void sona() {
