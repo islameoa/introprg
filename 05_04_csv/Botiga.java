bc7bb2741b902aa8d1e2d4ed45f6feb95325ae6c
islam <islameoa@gmail.com>
2022-04-20 13:23:21 +0200

    Exercici 05_04 fet

--- 05_04_csv/Botiga.java
+++ 05_04_csv/Botiga.java
@@ -1,19 +1,32 @@
-/*Classe botiga que contindrà un catàleg dels vins i  mòduls per afegir vins, cercar un vi, i eliminar vins del catàleg. Es pot inicialitzar una instància de la classe botiga amb un array amb el número de posicions que vulguem*/
+/*
+*Primera versio de la class Botiga, treballa amb arrays de vins, permet guardar nous vins als arrays i cercarlos dins dels arrays.
+*Actualment no disposa d'execucio propia.
+*/
 public class Botiga {
     private int DEFAULT_MAX_VINS = 10;
     private Vi vins[];
-
+    private int apuntador = -1;
+    
+    public int getApuntador(){
+    	return this.apuntador;
+    }
+    
+    public void setApuntador(int apuntador){
+    	this.apuntador = apuntador;
+    }
+    
+    //constructor de la classe botiga que seteja el maxim de vins a 10
     public Botiga() {
         vins = new Vi[DEFAULT_MAX_VINS];
     }
-
+    //constructor de la classe botiga que rep el nombre de vins màxim i si es major que zero crea el nou array
     public Botiga(int maxVins) {
         if (maxVins < 0) {
             maxVins = DEFAULT_MAX_VINS;
         }
         vins = new Vi[maxVins];
     }
-
+    //mòdul afegeix que afegeix un vi a l'array
     public Vi afegeix(Vi vi) {
         boolean elegible = true;
         if (vi.esValid()) {
@@ -35,7 +48,7 @@ public class Botiga {
         }
         return null;
     }
-
+    //mòdul cerca que rep el nom d'un vi i si el troba el retorna
     public Vi cerca(String nom) {
         nom = Vi.normalitzaNom(nom).toLowerCase();
         for (int i = 0; i < vins.length; i++) {
@@ -48,7 +61,7 @@ public class Botiga {
         }
         return null;
     }
-
+    //mòdul elimina que rep el nom d'un vi i l'elimina de l'array si el troba
     public Vi elimina(String nom) {
         nom = Vi.normalitzaNom(nom);
         for (int i = 0; i < vins.length; i++) {
@@ -66,5 +79,15 @@ public class Botiga {
         }
         return null;
     }
-
+    
+    //comença el recorregut dels vins de la botiga
+    public void iniciaRecorregut(){
+    	setApuntador(-1);
+    }
+    
+    //retorna el segÜent vi del recorregut de la botiga
+    public Vi getSeguent(){
+   		setApuntador(getApuntador() + 1);
+    	return vins[getApuntador()];
+    }
 }
---
+++ 05_04_csv/Vi.java
@@ -0,0 +1,126 @@
+/*Classe Vi que té com a propietats privades nom, preu i estoc i mètodes per construir una instància, setters i getters, metode toString per imprimir 
+la informació com volem i nomNormalitza que treu els espais al nom del vi, també té esValida, per saber si l'instància és vàlida */
+public class Vi {
+    private String nom;
+    private int preu;
+    private int estoc;
+
+    public Vi(String nom, int preu, int estoc) {
+        this.nom = normalitzaNom(nom);
+        if (preu < 0) {
+            this.preu = -1;
+        } else {
+            this.preu = preu;
+        }
+        if (estoc < 0) {
+            this.estoc = -1;
+        } else {
+            this.estoc = estoc;
+        }
+    }
+
+    public Vi(String nom, int preu) {
+        this.nom = normalitzaNom(nom);
+        if (preu < 0) {
+            this.preu = -1;
+        } else {
+            this.preu = preu;
+        }
+        this.estoc = 0;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPreu() {
+        return preu;
+    }
+
+    public int getEstoc() {
+        return estoc;
+    }
+
+    public void setPreu(int preu) {
+        if (preu >= 0) {
+            this.preu = preu;
+        }
+    }
+
+    public void setEstoc(int estoc) {
+        if (estoc >= 0) {
+            this.estoc = estoc;
+        }
+    }
+
+    //altres metodes
+        //asigna un format correcte per el vi
+    public static String normalitzaNom(String nom) {
+        try {
+            if (nom.isBlank()) {
+                throw new Exception("NOM NO VÀLID!");
+            }
+            nom = nom.strip();
+            String nomNormalitzat = "";
+            boolean espai = false;
+            for (int i = 0; i < nom.length(); i++) {
+                if (nom.charAt(i) != ' ') {
+                    nomNormalitzat += nom.charAt(i);
+                    espai = false;
+                }
+                if ((nom.charAt(i) == ' ') && (!espai)) {
+                    nomNormalitzat += ' ';
+                    espai = true;
+                }
+            }
+            return nomNormalitzat;
+        } catch (Exception e) {
+            return "NOM NO VÀLID!";
+        }
+    }
+        //comprova si un vi es valid
+    public boolean esValid() {
+        return (nom != null && nom != "NOM NO VÀLID!" && nom.length() > 0 && preu >= 0 && estoc >= 0);
+    }
+
+        //format del vi per ser mostrat com String
+    @Override
+    public String toString() {
+        return "\n    Vi: " + getNom() + "\n    Preu: " + getPreu() + "\n    Estoc: " + getEstoc() + "\n";
+    }
+    
+    //rep un array de Strings amb els atributs del vi i retorna un vi amb aquests atributs
+    public static Vi deArrayString(String[] vi){
+        if (vi.length != 3) {
+            return null;
+        }
+        try {
+            int preu = Integer.parseInt(vi[1]);
+            int estoc = Integer.parseInt(vi[2]);
+            return new Vi(vi[0], preu, estoc);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    //rep una instància de vi i retorna un array de strings amb els atributs del vi
+    public static String[] aArrayString(Vi vino){
+        String[] vi = new String[3];
+        vi[0] = vino.getNom();
+        vi[1] = Integer.toString(vino.getPreu());
+        vi[2] = Integer.toString(vino.getEstoc());
+        return vi;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
