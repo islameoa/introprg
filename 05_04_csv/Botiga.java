6757c329b4dccdee4d5690ce7560898e6261fae7
prgtest <islameoa@gmail.com>
2022-04-25 12:28:43 +0200

    prgtest autocommit #14 05_04: Exercici 05_04 fet

--- 05_04_csv/Botiga.java
+++ 05_04_csv/Botiga.java
@@ -7,26 +7,29 @@ public class Botiga {
     private Vi vins[];
     private int apuntador = -1;
 
-    public int getApuntador(){
-    	return this.apuntador;
+    public int getApuntador() {
+        return this.apuntador;
     }
-    
-    public void setApuntador(int apuntador){
-    	this.apuntador = apuntador;
+
+    public void setApuntador(int apuntador) {
+        this.apuntador = apuntador;
     }
-    
-    //constructor de la classe botiga que seteja el maxim de vins a 10
+
+    // constructor de la classe botiga que seteja el maxim de vins a 10
     public Botiga() {
         vins = new Vi[DEFAULT_MAX_VINS];
     }
-    //constructor de la classe botiga que rep el nombre de vins màxim i si es major que zero crea el nou array
+
+    // constructor de la classe botiga que rep el nombre de vins màxim i si es major
+    // que zero crea el nou array
     public Botiga(int maxVins) {
         if (maxVins < 0) {
             maxVins = DEFAULT_MAX_VINS;
         }
         vins = new Vi[maxVins];
     }
-    //mòdul afegeix que afegeix un vi a l'array
+
+    // mòdul afegeix que afegeix un vi a l'array
     public Vi afegeix(Vi vi) {
         boolean elegible = true;
         if (vi.esValid()) {
@@ -48,7 +51,8 @@ public class Botiga {
         }
         return null;
     }
-    //mòdul cerca que rep el nom d'un vi i si el troba el retorna
+
+    // mòdul cerca que rep el nom d'un vi i si el troba el retorna
     public Vi cerca(String nom) {
         nom = Vi.normalitzaNom(nom).toLowerCase();
         for (int i = 0; i < vins.length; i++) {
@@ -61,9 +65,10 @@ public class Botiga {
         }
         return null;
     }
-    //mòdul elimina que rep el nom d'un vi i l'elimina de l'array si el troba
+
+    // mòdul elimina que rep el nom d'un vi i l'elimina de l'array si el troba
     public Vi elimina(String nom) {
-        //ordena();
+        // ordena();
         nom = Vi.normalitzaNom(nom);
         for (int i = 0; i < vins.length; i++) {
             if (vins[i] != null) {
@@ -80,40 +85,40 @@ public class Botiga {
         }
         return null;
     }
-    
-    //comença el recorregut dels vins de la botiga
-    public void iniciaRecorregut(){
-    	setApuntador(-1);
+
+    // comença el recorregut dels vins de la botiga
+    public void iniciaRecorregut() {
+        setApuntador(-1);
     }
-    
-    //retorna el segÜent vi del recorregut de la botiga
-    public Vi getSeguent(){
-   		if (getApuntador() < vins.length){
+
+    // retorna el segÜent vi del recorregut de la botiga
+    public Vi getSeguent() {
+        if (getApuntador() < vins.length) {
             setApuntador(getApuntador() + 1);
         }
-        if(!estaBuida()){
-            while (vins[getApuntador()] == null){
-                setApuntador(getApuntador() + 1);
+
+        while (vins[getApuntador()] == null && getApuntador() < vins.length) {
+            setApuntador(getApuntador() + 1);
+        }
+
+        return vins[getApuntador()];
+    }
+
+    public Vi getActual() {
+        return vins[0];
+    }
+
+    // modulo que mira si la botiga esta buida
+    public boolean estaBuida() {
+        for (int i = 0; i < vins.length; i++) {
+            if (vins[i] != null) {
+                return false;
             }
         }
-    	return vins[getApuntador()];
+        return true;
     }
 
-    public Vi getActual(){
-    	return vins[0];
-    }
-
-    //modulo que mira si la botiga esta buida
-    public boolean estaBuida(){
-    	for (int i = 0; i < vins.length; i++){
-    		if (vins[i] != null){
-    			return false;
-    		}
-    	}
-    	return true;
-    }
-
-    //modulo que ordena el array de vinos
+    // modulo que ordena el array de vinos
     public void ordena() {
         for (int i = 0; i < vins.length; i++) {
             for (int j = i + 1; j < vins.length; j++) {
