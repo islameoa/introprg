5be01970d34bc50644828b7ea6e7869f4cfe2765
islam <islameoa@gmail.com>
2022-04-27 17:22:00 +0200

    Exercici 05_10 fet

--- 05_10_gatposicio/GatPosicio.java
+++ 05_10_gatposicio/GatPosicio.java
@@ -1,7 +1,7 @@
 /*Enumerat GatPosicio que conté les 3 possibles posicions i el mètode fromString per passar de string a enu*/
 public enum GatPosicio {
     DRET, ASSEGUT, ESTIRAT;
-    @Override
+    
     public static GatPosicio fromString(String nom) {
         for (GatPosicio posicio: GatPosicio.values()) {
             if (posicio.name().equalsIgnoreCase(nom)) return posicio;
