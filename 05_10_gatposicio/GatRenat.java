114c3159b0c26a9394c09b1ad1e46fbdf1dafdf0
islam <islameoa@gmail.com>
2022-04-27 18:26:34 +0200

    Exercici 05_10 fet

--- 05_10_gatposicio/GatRenat.java
+++ 05_10_gatposicio/GatRenat.java
@@ -1,29 +1,37 @@
+import java.security.PublicKey;
+
 /*Exercici en el que declarem la classe GatRenat que tindrà com a propietat posició i diversos mòduls per gestionar-la*/
 
 public class GatRenat {
     private GatPosicio posicio = GatPosicio.ESTIRAT;
-    public GatRenat(GatPosicio pos){
+
+    public GatRenat() {
+
+    }
+
+    public GatRenat(GatPosicio pos) {
         posicio = pos;
     }
 
-    public GatRenat(String pos){
+    public GatRenat(String pos) {
         posicio = GatPosicio.fromString(pos);
     }
 
-    public GatPosicio getPosicio(){
+    public GatPosicio getPosicio() {
         return posicio;
     }
 
-    public void setPosicio(GatPosicio pos){
+    public void setPosicio(GatPosicio pos) {
         posicio = pos;
         // try {
-        //     posicio = pos;
+        // posicio = pos;
         // } catch (Exception e) {
-        //     throw new IllegalArgumentException(String.format("Nom \"%s\" no suportat per GatPosicio", pos));
+        // throw new IllegalArgumentException(String.format("Nom \"%s\" no suportat per
+        // GatPosicio", pos));
         // }
     }
 
-    public void setPosicio(String pos){
+    public void setPosicio(String pos) {
         posicio = GatPosicio.fromString(pos);
     }
 }
