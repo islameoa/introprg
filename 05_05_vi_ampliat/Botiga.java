fc71bf2b410eb5a14a10f034ae882f6a034bee84
prgtest <islameoa@gmail.com>
2022-05-08 13:04:25 +0200

    prgtest autocommit #10 05_05: Merge branch 'main' of github.com:islameoa/introprg into main

--- 05_05_vi_ampliat/Botiga.java
+++ 05_05_vi_ampliat/Botiga.java
@@ -103,10 +103,10 @@ public class Botiga {
                 if (plantilla.getNom() != null && !plantilla.getNom().equalsIgnoreCase(vi.getNom()))
                     continue;
 
-                if (plantilla.getPreu() >= 0 && plantilla.getPreu() < vi.getPreu())
+                if (plantilla.getPreu() >= 0 && plantilla.getPreu() <= vi.getPreu())
                     continue;
 
-                if (plantilla.getEstoc() >= 0 && plantilla.getEstoc() > vi.getEstoc())
+                if (plantilla.getEstoc() >= 0 && plantilla.getEstoc() >= vi.getEstoc())
                     continue;
 
                 if (plantilla.getTipus() != null && !plantilla.getTipus().equalsIgnoreCase(vi.getTipus()))
