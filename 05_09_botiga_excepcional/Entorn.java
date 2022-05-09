521caa347e7213f58616854678a82ab86cb0eca3
prgtest <islameoa@gmail.com>
2022-05-09 13:27:03 +0200

    prgtest autocommit #1 05_09: exercici 05_09 fet

--- 05_09_botiga_excepcional/Entorn.java
+++ 05_09_botiga_excepcional/Entorn.java
@@ -241,24 +241,46 @@ public class Entorn {
     }
 
     public static void getReferencies() throws Exception {
-        BufferedReader input = new BufferedReader(new FileReader("botiga.csv"));
-        String line = input.readLine();
-        int contador = 0;
-        while (line != null) {
-            String[] parts = line.split(";");
-            if (parts.length == 8 && UtilString.esEnter(parts[2]) && UtilString.esEnter(parts[3])
-                    && UtilString.esEnter(parts[7])) {
-                if (UtilString.esEnter(parts[2]) && UtilString.esEnter(parts[3]) && UtilString.esEnter(parts[7])) {
-                    botiga.afegeix(Vi.deArrayString(parts));
-                    contador ++;
+        // BufferedReader input = new BufferedReader(new FileReader("botiga.csv"));
+        // String line = input.readLine();
+        // int contador = 0;
+        // while (line != null) {
+        //     String[] parts = line.split(";");
+        //     if (parts.length == 8 && UtilString.esEnter(parts[2]) && UtilString.esEnter(parts[3])
+        //             && UtilString.esEnter(parts[7])) {
+        //         if (UtilString.esEnter(parts[2]) && UtilString.esEnter(parts[3]) && UtilString.esEnter(parts[7])) {
+        //             botiga.afegeix(Vi.deArrayString(parts));
+        //             contador ++;
+        //         }
+        //     }
+        //     line = input.readLine();
+        //     if (contador == 9){
+        //         throw new Exception("ERROR: massa entrades a botiga.csv");
+        //     }
+        // }
+        // input.close();
+        try {
+            BufferedReader input = new BufferedReader(new FileReader("botiga.csv"));
+            String line = input.readLine();
+            int contador = 0;
+            while (line != null) {
+                String[] parts = line.split(";");
+                if (parts.length == 8 && UtilString.esEnter(parts[2]) && UtilString.esEnter(parts[3])
+                        && UtilString.esEnter(parts[7])) {
+                    if (UtilString.esEnter(parts[2]) && UtilString.esEnter(parts[3]) && UtilString.esEnter(parts[7])) {
+                        botiga.afegeix(Vi.deArrayString(parts));
+                        contador ++;
+                    }
+                }
+                line = input.readLine();
+                if (contador == 9){
+                    throw new Exception("ERROR: massa entrades a botiga.csv");
                 }
             }
-            line = input.readLine();
-            if (contador == 9){
-                throw new Exception("ERROR: massa entrades a botiga.csv");
-            }
+            input.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        input.close();
     }
 
     // metodo booleano que mira si existe el fichero botiga.txt
