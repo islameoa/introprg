71fad58bb02219c2146216dba2ee4da3edf32819
islam <islameoa@gmail.com>
2022-05-15 23:44:18 +0200

    Exercici 52_03 fet

---
+++ 52_03_coordenada_y/UsaPunt.java
@@ -0,0 +1,9 @@
+/*Classe UsaPunt que ens crearà un punt i ens retornara les seves coordenadesz.*/
+
+ public class UsaPunt {
+     public static void main(String[] args){
+         Punt punt = new Punt();
+         System.out.printf("punt.getX() -> %d%n", punt.getX());
+         System.out.printf("punt.getY() -> %d%n", punt.getY());
+     }
+ }
