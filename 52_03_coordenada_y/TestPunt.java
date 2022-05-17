2ba9b3ed70ade1277430e6e6b844d032d17cb067
islam <islameoa@gmail.com>
2022-05-17 09:19:59 +0200

    exercici 52_03 fet

--- 52_03_coordenada_y/TestPunt.java
+++ 52_03_coordenada_y/TestPunt.java
@@ -1,10 +1,23 @@
-/*Classe TestPunt que ens crearà el seu test,    h simplement declara un nou punt.*/
-
+/*Classe TestPunt que ens crearà el seu test, simplement declara un nou punt.*/
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 public class TestPunt {
     @Test
     public void constructor() {
         // comprova que Punt() pugui ser cridat
         new Punt();
     }
+    @Test
+    public void constructorDefecteXZero() {
+        // comprova que Punt() deixi a 0 la propietat x de Punt
+        Punt p = new Punt();
+        Assertions.assertEquals(0, p.getX());
+    }
+    @Test
+    public void constructorDefecteYZero() {
+        // comprova que Punt() deixi a 0 la propietat y de Punt
+        Punt p = new Punt();
+        Assertions.assertEquals(0, p.getY());
+    }
+
 }
