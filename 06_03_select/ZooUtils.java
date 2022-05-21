6e89511198754f289b07ac617474f0abf822da90
islam <islameoa@gmail.com>
2022-05-21 11:25:06 +0200

    exercici 06_03 fet

---
+++ 06_03_select/ZooUtils.java
@@ -0,0 +1,10 @@
+/***Classe Zoo Utils que contindrà mètodes per a facilitar la interacció entre Zoo i usa zoo */
+import java.util.List;
+
+public class ZooUtils {
+    public static void mostraCategories(List<Categoria> categories) {
+        for (Categoria categoria : categories) {
+            System.out.println("\t" + categoria);
+        }
+    }
+}
