bafef956348f499064b174b432cadb5d27be1d75
prgtest <islameoa@gmail.com>
2022-05-21 11:32:53 +0200

    prgtest autocommit #4 06_03: exercici 06_03 fet

--- 06_03_select/ZooUtils.java
+++ 06_03_select/ZooUtils.java
@@ -3,15 +3,14 @@ import java.util.List;
 
 public class ZooUtils {
     public static void mostraCategories(List<Categoria> categories) {
-        int contador = 0;
-        for (Categoria categoria : categories) {
-            System.out.println("\t" + categoria);
-            contador++;
-        }
         if (categories.isEmpty()) {
             System.out.println("Cap categoria");
         } else {
-            System.out.println("Nombre de categories: " + contador);
+            System.out.println("Nombre de categories: " + categories.size());
         }
+        for (Categoria categoria : categories) {
+            System.out.println("\t" + categoria);
+        }
+        
     }
 }
