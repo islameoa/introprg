ad0c3152f4894a1586c5082276522e647dcc3b3f
islam <islameoa@gmail.com>
2022-05-24 15:19:38 +0200

    exercici 06_05 fet

---
+++ 06_05_animals/Animal.java
@@ -0,0 +1,44 @@
+public class Animal {
+    private int id = -1;  // -1 indica no assignat/indefinit
+    private String nom;
+    private Categoria categoria;
+
+    public Animal(String nom, Categoria categoria) {
+        if (nom == null || nom.isBlank()) {
+            throw new IllegalArgumentException("El nom no pot ser null ni blanc");
+        }
+        if (categoria == null) {
+            throw new IllegalArgumentException("La categoria no pot ser null");
+        }
+        this.nom = nom;
+        this.categoria = categoria;
+    }
+
+    public Animal(int id, String nom, Categoria categoria) {
+        this(nom, categoria);
+        if (id < 0) {
+            throw new IllegalArgumentException("L'identificador ha de ser positiu");
+        }
+        this.id = id;
+    }
+
+    public boolean idIndefinit() { return id < 0; }
+
+    public int getId() {
+        if (idIndefinit()) {
+            throw new UnsupportedOperationException("L'identificador no està disponible");
+        }
+        return id;
+    }
+
+    public String getNom() { return nom; }
+
+    public Categoria getCategoria() { return categoria; }
+
+    @Override
+    public String toString() {
+        return "Animal(id:" +
+            (id < 0 ? "indefinit" : id) +
+            ", " + nom + ", " + categoria + ")";
+    }
+}
---
+++ 06_05_animals/ZooUtils.java
@@ -0,0 +1,15 @@
+/***Classe Zoo Utils que contindrà mètodes per a facilitar la interacció entre Zoo i usa zoo */
+import java.util.List;
+public class ZooUtils {
+    public static void mostraCategories(List<Categoria> categories) {
+        if (categories.isEmpty()) {
+            System.out.println("Cap categoria");
+        } else {
+            System.out.println("Nombre de categories: " + categories.size());
+        }
+        for (Categoria categoria : categories) {
+            System.out.println("\t" + categoria);
+        }
+        
+    }
+}
